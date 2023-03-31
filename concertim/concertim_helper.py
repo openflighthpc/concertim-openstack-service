d52ef5851a63377408ff5e5ceefb5b3ba40fdc00
Logan Smith <logan.smith8426@gmail.com>
2023-03-31 17:10:26 +0100

    demo stuff

---
+++ concertim/concertim_helper.py
@@ -0,0 +1,162 @@
+import time
+import json
+import requests
+import logging
+import os
+
+# Returns the list of templates currently in CONCERTIM
+def get_concertim_templates(concertimService):
+    base_url = concertimService._config["concertim_url"]
+    url = f"{base_url}/api/v1/templates"
+    headers = {"Accept": "application/json"}
+    if concertimService._auth_token is not None:
+        headers["Authorization"] = concertimService._auth_token
+    else:
+        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        concertimService._logger.info("Retrieved Templates Successfully")
+        return response.json()
+    except Exception as e:
+        concertimService._logger.exception("Failed to retrieve templates from CONCERTIM API: %s", e)
+        raise e
+        return
+
+# Returns the list of devices currently in CONCERTIM
+def get_concertim_devices(concertimService):
+    base_url = concertimService._config["concertim_url"]
+    url = f"{base_url}/api/v1/devices"
+    headers = {"Accept": "application/json"}
+    if concertimService._auth_token is not None:
+        headers["Authorization"] = concertimService._auth_token
+    else:
+        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        concertimService._logger.info("Retrieved Devices Successfully")
+        return response.json()
+    except Exception as e:
+        concertimService._logger.exception("Failed to retrieve devices from CONCERTIM API: %s", e)
+        raise e
+        return
+
+# Returns the list of racks currently in CONCERTIM
+def get_concertim_racks(concertimService):
+    base_url = concertimService._config["concertim_url"]
+    url = f"{base_url}/api/v1/racks"
+    headers = {"Accept": "application/json"}
+    if concertimService._auth_token is not None:
+        headers["Authorization"] = concertimService._auth_token
+    else:
+        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        concertimService._logger.info("Retrieved Racks Successfully")
+        return response.json()
+
+    except Exception as e:
+        concertimService._logger.exception("Failed to retrieve racks from CONCERTIM API: %s", e)
+        raise e
+        return
+
+# Returns the information of a device currently in CONCERTIM based on ID
+def show_concertim_device(concertimService, device_id):
+    base_url = url
+    url = f"{base_url}/api/v1/devices/{device_id}"
+    headers = {"Accept": "application/json"}
+    if concertimService._auth_token is not None:
+        headers["Authorization"] = concertimService._auth_token
+    else:
+        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        concertimService._logger.info("Retrieved Device Information Successfully for device: %s", device_id)
+        return response.json()
+    except Exception as e:
+        concertimService._logger.exception("Failed to retrieve device information from CONCERTIM API: %s", e)
+        raise e
+        return
+
+# Returns the information of a rack currently in CONCERTIM based on ID
+def show_concertim_rack(concertimService, rack_id):
+    base_url = concertimService._config["concertim_url"]
+    url = f"{base_url}/api/v1/racks/{rack_id}"
+    headers = {"Accept": "application/json"}
+    if concertimService._auth_token is not None:
+        headers["Authorization"] = concertimService._auth_token
+    else:
+        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        concertimService._logger.info("Retrieved Rack Information Successfully for rack: %s", rack_id)
+        return response.json()
+
+    except Exception as e:
+        concertimService._logger.exception("Failed to retrieve rack information from CONCERTIM API: %s", e)
+        raise e
+        return
+
+# Create a device in CONCERTIM with the passed args (facing is either f or b for front or back respectively, default will be front)
+def create_concertim_device(concertimService, device_name, rack_id, start_location_id, template_id, device_description, facing='f'):
+    base_url = concertimService._config["concertim_url"]
+    url = f"{base_url}/api/v1/nodes"
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    data = json.dumps({
+        "template_id": template_id,
+        "device": {
+            "name": device_name,
+            "description": device_description,
+            "location": {
+                "facing": facing,
+                "rack_id": rack_id,
+                "start_u": start_location_id
+            }
+        }
+    })
+    if concertimService._auth_token is not None:
+        headers["Authorization"] = concertimService._auth_token
+    else:
+        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+    try:
+        response = requests.post(url, headers=headers, data=data, verify=False)
+        if response.status_code in (200, 201):
+            concertimService._logger.info("New Device Created Successfully")
+            return response.json()
+        else:
+            raise Exception("Response returned non 200 or 201 status code: %s", response.text)
+    except Exception as e:
+        concertimService._logger.exception("Failed to create new device in CONCERTIM API: %s", e)
+
+# Post a metric to concertim for a specific object (rack, device, etc)
+#   obj_to_update_name  =   the name of the rack/device/etc in concertim
+#   metric_name         =   the name of the metric to put in concertim
+#   metric_value        =   value of above metric
+#   metric_datatype     =   datatype for the metric_value
+#   metric_units        =   units for above metric
+#   metric_slope        =   for constant metric slope is "zero"    for metric that changes value slope is "both"
+#   metric_ttl          =   not sure, some time thing, ttl default is 3600
+def post_metric_to_concertim(concertimService, obj_to_update_name, metric_name, metric_value, metric_datatype, metric_units=" ", metric_slope="both", metric_ttl=3600):
+    base_url = concertimService._config["concertim_url"]
+    url = f"{base_url}/mrd/{obj_to_update_name}/metrics"
+    headers = {"Content-Type": "application/json"}
+    data = json.dumps({
+        "type": metric_datatype, 
+        "name": metric_name, 
+        "value": metric_value, 
+        "units": metric_units, 
+        "slope": metric_slope, 
+        "ttl": metric_ttl
+    })
+    if concertimService._auth_token is not None:
+        headers["Authorization"] = concertimService._auth_token
+    else:
+        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: %s", concertimService._auth_token)
+    try:
+        response = requests.put(url, headers=headers, data=data, verify=False)
+        concertimService._logger.info("Metric Successfully Added: added %s to %s", metric_name, obj_to_update_name)
+        return response.json()
+    except Exception as e:
+        concertimService._logger.exception("Failed to put metric in CONCERTIM: %s", e)
+        raise e
+        return
+        
--- concertim/concertim_service.py
+++ concertim/concertim_service.py
@@ -1,15 +1,17 @@
 # TODO:
 # change 'device' logic to bridge instances in openstack and devices in concertim
-# check gnocchi logic
 
 import time
 import json
 import requests
+requests.packages.urllib3.disable_warnings() 
 import logging
 import os
+from datetime import datetime, timedelta
 import gnocchiclient.v1.client as gnocchi_client
 from keystoneauth1 import session
 from keystoneauth1.identity import v3
+import concertim_helper
 
 
 # The main service class
@@ -31,10 +33,27 @@ class ConcertimService(object):
         self._connect_gnocchi()
         self._authenticate_concertim(self._config["concertim_username"], self._config["concertim_password"])
         #while True:
-        #    self._update_devices()
         #    self._send_metrics()
         #    time.sleep(self._config["interval"])
-        #self._update_devices()
+        
+        #print(concertim_helper.get_concertim_devices(concertimService=self))
+        #print(concertim_helper.show_concertim_device(concertimService=self, device_id=6))
+        #print(concertim_helper.get_concertim_templates(concertimService=self))
+        #print(concertim_helper.create_concertim_device(concertimService=self, device_name="test-device-1", 
+        #                                               rack_id=1, start_location_id=30, template_id=2, 
+        #                                               device_description="Testing creating from python concertim openstack service", 
+        #                                               facing="f"))
+        #print(concertim_helper.create_concertim_device(concertimService=self, device_name="test-device-2", 
+        #                                               rack_id=1, start_location_id=20, template_id=4, 
+        #                                               device_description="Testing creating from python concertim openstack service", 
+        #                                               facing="f"))
+        #print(concertim_helper.post_metric_to_concertim(concertimService=self, obj_to_update_name="test-device-1",
+        #                                                metric_name="os.test.metric", metric_value=5, metric_datatype="int32", metric_slope="both"))
+        #print(self._gnocchi.status.get(details=True))
+        #print(self._gnocchi.resource.list(sorts=["project_id:desc"]))
+        #print(self._gnocchi.metric.list(limit=1))
+        self._send_metrics()
+
 
     # Loads the configuration from the specified JSON file
     def _load_config(self, config_file):
@@ -55,19 +74,18 @@ class ConcertimService(object):
 
     # Authenticates with the OpenStack Keystone API and obtains an auth object
     def _authenticate_openstack(self):
-        auth_url = self._config["auth_url"]
-        auth = v3.Password(auth_url=auth_url,
+        auth = v3.Password(auth_url=self._config["auth_url"],
                            username=self._config["username"],
                            password=self._config["password"],
                            project_name=self._config["project_name"],
-                           user_domain_id=self._config["user_domain_name"],
-                           project_domain_id=self._config["project_domain_name"])
-        self._auth = auth        
+                           user_domain_name=self._config["user_domain_name"],
+                           project_domain_name=self._config["project_domain_name"])
+        self._auth = auth
         
     # Authenticates with the CONCERTIM API and obtains an authentication token
     def _authenticate_concertim(self, login, password):
         base_url = self._config["concertim_url"]
-        url = f"{base_url}users/sign_in.json"
+        url = f"{base_url}/users/sign_in.json"
         headers = {"Content-Type": "application/json", "Accept": "application/json"}
         data = json.dumps({
             "user": {
@@ -75,11 +93,7 @@ class ConcertimService(object):
                 "password": password,
             }
         })
-        print(f"URL: {url}")
-        print(f"Headers: {headers}")
-        print(f"Data: {data}")
-        response = requests.post(url, headers=headers, json=data)
-        print(response.json())
+        response = requests.post(url, headers=headers, data=data, verify=False)
         if response.status_code in (200, 201):
             token = response.headers.get("Authorization")
             if token:
@@ -99,63 +113,33 @@ class ConcertimService(object):
             gnocchi = gnocchi_client.Client(session=sess)
             self._gnocchi = gnocchi
         except Exception as e:
-            self._logger.error("Failed to authenticate with OpenStack: {}".format(str(e)))
+            self._logger.error("Failed to authenticate Gnocchi with OpenStack: {}".format(str(e)))
             raise e
-        self._logger.info("Authenticated successfully")
-
-    # Updates the devices from the CONCERTIM API
-    def _update_devices(self):
-        devices = self._config["devices"]
-        for device in devices:
-            try:
-                device_id = device["device_id"]
-                response = requests.get(self._config["concertim_url"] + "/" + device_id, headers={"X-Auth-Token": self._auth_token})
-                if response.status_code == 200:
-                    device_data = response.json()
-                    self._devices[device_id] = device_data
-                else:
-                    self._logger.error("Failed to get device data for device %s", device_id)
-            except Exception as e:
-                self._logger.error("Failed to update device %s: %s", device_id, e)
+        self._logger.info("Authenticated and connected Gnocchi successfully")
 
     # Sends the metrics for each device to the CONCERTIM API
     def _send_metrics(self):
-        for device_id, device_data in self._devices.items():
-            metrics = device_data.get("metrics", [])
-            if not metrics:
-                self._logger.warning("No metrics found for device %s", device_id)
-                continue
+        query_resource = {"=":{"project_id":"ef821aa9e576420c8768671d911a9766"}}
+        resource_list = self._gnocchi.resource.search(resource_type="instance",query=query_resource)
+        #print(resource_list)
+        metrics = resource_list[0]['metrics']
+        vcpus_details = self._gnocchi.metric.get(metric=metrics['cpu'])
+        #print(vcpus_details)
+        now = datetime.now()
+        hour_ago = now - timedelta(hours=1)
+        vcpu_amount = self._gnocchi.metric.get_measures(metric=metrics['cpu'], start=hour_ago, stop=now)
+        print(vcpu_amount)
 
-            try:
-                # Build the Gnocchi query to fetch aggregated metric data
-                query = {"=": {"device_id": device_id}}
-                for metric in metrics:
-                    query["="][metric["name"]] = metric.get("filter")
-
-                # Query Gnocchi for aggregated metric data
-                metric_data = self._gnocchi.metric.get_measures(
-                    metric=metrics[0]["name"],
-                    query=query,
-                    aggregation=metrics[0].get("aggregation", "mean"),
-                    granularity=metrics[0].get("granularity", 300),
-                    start="now-1h",
-                    stop="now"
-                )
-
-                # Convert the metric data to the format expected by the CONCERTIM API
-                metric_values = []
-                for timestamp, value in metric_data:
-                    metric_values.append({"timestamp": timestamp, "value": value})
-                metrics_data = {"device_id": device_id, "metrics": [{ "name": metrics[0]["name"], "values": metric_values }]}
-                headers = {"X-Auth-Token": self._auth_token, "Content-Type": "application/json"}
-                response = requests.post(self._config["concertim_url"], headers=headers, json=metrics_data)
-
-                if response.status_code == 200:
-                    self._logger.info("Sent metrics for device %s", device_id)
-                else:
-                    self._logger.error("Failed to send metrics for device %s: %s", device_id, response.text)
-            except Exception as e:
-                self._logger.error("Failed to send metrics for device %s: %s", device_id, e)
+        #for resource in resource_list:
+        #    for metric in resource['metrics']:
+        #        unit = self._gnocchi.metric.get(metric=metrics[metric])['unit']
+        #        amount = int(self._gnocchi.metric.get_measures(metric=metrics[metric])[0][2])
+        #        print(f"Amount is {amount} for {metric} in {unit}")
+        #        if metric == "vcpus":
+        #            concertim_helper.post_metric_to_concertim(concertimService=self, obj_to_update_name="test-device-1", 
+        #                                                    metric_name="os."+metric, metric_value=amount, metric_datatype="int32", 
+        #                                                    metric_slope="zero", metric_units=unit)
+        #            print("Sent vcpus metric to test-device-1")
 
     # Handles exceptions that occur during runtime
     def _handle_exception(self, e):
