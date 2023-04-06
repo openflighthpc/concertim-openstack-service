c448ba92964ec4d250b445ce8a42d179f9452c0a
Logan Smith <logan.smith8426@gmail.com>
2023-04-06 13:25:09 +0100

    Metrics and instances

--- concertim/concertim_helper.py
+++ concertim/concertim_helper.py
@@ -10,13 +10,13 @@ def get_concertim_templates(concertimService):
     if concertimService._auth_token is not None:
         headers["Authorization"] = concertimService._auth_token
     else:
-        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+        raise Exception(f"No Authentication Token found in concertimService object - concertimService._auth_token is: {concertimService._auth_token}")
     try:
         response = requests.get(url, headers=headers, verify=False)
         concertimService._logger.info("Retrieved Templates Successfully")
         return response.json()
     except Exception as e:
-        concertimService._logger.exception("Failed to retrieve templates from CONCERTIM API: ", e)
+        concertimService._logger.exception(f"Failed to retrieve templates from CONCERTIM API: {e}")
         raise e
         return
 
@@ -28,13 +28,13 @@ def get_concertim_devices(concertimService):
     if concertimService._auth_token is not None:
         headers["Authorization"] = concertimService._auth_token
     else:
-        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+        raise Exception(f"No Authentication Token found in concertimService object - concertimService._auth_token is: {concertimService._auth_token}")
     try:
         response = requests.get(url, headers=headers, verify=False)
         concertimService._logger.info("Retrieved Devices Successfully")
         return response.json()
     except Exception as e:
-        concertimService._logger.exception("Failed to retrieve devices from CONCERTIM API: ", e)
+        concertimService._logger.exception(f"Failed to retrieve devices from CONCERTIM API: {e}")
         raise e
         return
 
@@ -46,14 +46,14 @@ def get_concertim_racks(concertimService):
     if concertimService._auth_token is not None:
         headers["Authorization"] = concertimService._auth_token
     else:
-        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+        raise Exception(f"No Authentication Token found in concertimService object - concertimService._auth_token is: {concertimService._auth_token}")
     try:
         response = requests.get(url, headers=headers, verify=False)
         concertimService._logger.info("Retrieved Racks Successfully")
         return response.json()
 
     except Exception as e:
-        concertimService._logger.exception("Failed to retrieve racks from CONCERTIM API: ", e)
+        concertimService._logger.exception(f"Failed to retrieve racks from CONCERTIM API: {e}")
         raise e
         return
 
@@ -65,13 +65,13 @@ def show_concertim_device(concertimService, device_id):
     if concertimService._auth_token is not None:
         headers["Authorization"] = concertimService._auth_token
     else:
-        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+        raise Exception(f"No Authentication Token found in concertimService object - concertimService._auth_token is: {concertimService._auth_token}")
     try:
         response = requests.get(url, headers=headers, verify=False)
-        concertimService._logger.info("Retrieved Device Information Successfully for device: ", device_id)
+        concertimService._logger.info(f"Retrieved Device Information Successfully for device: {device_id}")
         return response.json()
     except Exception as e:
-        concertimService._logger.exception("Failed to retrieve device information from CONCERTIM API: ", e)
+        concertimService._logger.exception(f"Failed to retrieve device information from CONCERTIM API: {e}")
         raise e
         return
 
@@ -83,16 +83,89 @@ def show_concertim_rack(concertimService, rack_id):
     if concertimService._auth_token is not None:
         headers["Authorization"] = concertimService._auth_token
     else:
-        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+        raise Exception(f"No Authentication Token found in concertimService object - concertimService._auth_token is: {concertimService._auth_token}")
     try:
         response = requests.get(url, headers=headers, verify=False)
-        concertimService._logger.info("Retrieved Rack Information Successfully for rack: ", rack_id)
+        concertimService._logger.info(f"Retrieved Rack Information Successfully for rack: {rack_id}")
         return response.json()
 
     except Exception as e:
-        concertimService._logger.exception("Failed to retrieve rack information from CONCERTIM API: ", e)
+        concertimService._logger.exception(f"Failed to retrieve rack information from CONCERTIM API: {e}")
         raise e
         return
+
+# Gets the template that should be used given the number of instance vcpus
+def get_device_template(concertimService, instance_vcpus):
+    available_templates = get_concertim_templates(concertimService)
+    concertim_template = None
+    for template in available_templates:
+        if str(f"{instance_vcpus} VCPU") in template['description']:
+            concertim_template = template
+    if not concertim_template:
+        # default to small
+        concertim_template = available_templates[1]
+    return concertim_template
+
+# Finds an empty spot in the available racks that can fit the new device
+def find_spot_in_rack(concertimService, device_template, occupied_rows_all_racks):
+    new_rack_id = -1
+    start_location = -1
+    new_device_size = -1
+    spot_found = False
+    end_location = -1
+    if device_template['name'] == "Small":
+        new_device_size=1
+    if device_template['name'] == "Medium":
+        new_device_size=2
+    if device_template['name'] == "Large":
+        new_device_size=3
+    if device_template['name'] == "Xlarge":
+        new_device_size=4
+    for rack in occupied_rows_all_racks:
+        if spot_found:
+            break
+        rack_details = show_concertim_rack(concertimService, rack)
+        rack_height = rack_details['u_height']
+        for rack_row in range(rack_height, 0, -1):
+            if (rack_row + new_device_size - 1) <= rack_height and rack_row >= 1:
+                fits = True
+                for device_space in range(0, new_device_size):
+                    row = (rack_row + device_space)
+                    if row in occupied_rows_all_racks[rack]:
+                        fits = False
+                if fits:
+                    new_rack_id = rack
+                    start_location = rack_row
+                    end_location = (rack_row + new_device_size - 1)
+                    spot_found= True
+                    break
+    if spot_found:
+        concertimService._logger.info(f"Empty Rack Location found")
+        return {"rack_id":new_rack_id, "start_u":start_location, "end_u":end_location}
+    else:
+        # build new rack
+        return
+
+# Build a concertim device from an openstack instance
+def build_device_list(concertimService, device_list):
+    concertimService._logger.info(f"Building New Devices in Concertim")
+    rack_list = get_concertim_racks(concertimService)
+    occupied_rows_all_racks = {}
+    for rack in rack_list:
+        rack_details = show_concertim_rack(concertimService, rack['id'])
+        devices = rack_details['devices']
+        occupied_rows = []
+        for device in devices:
+            occupied_rows.extend(list(range(device['location']['start_u'], device['location']['end_u']+1)))
+        occupied_rows_all_racks[rack['id']] = occupied_rows
+    
+    for device in device_list:
+        concertimService._logger.info(f"Finding Location for {device[0]}")
+        new_device_template = get_device_template(concertimService, device[2])
+        new_device_location = find_spot_in_rack(concertimService, new_device_template, occupied_rows_all_racks)
+        concertimService._logger.debug(f"Building {device[0]} with template {new_device_template} at location {new_device_location}")
+        occupied_rows_all_racks[rack['id']].extend(list(range(new_device_location['start_u'], new_device_location['end_u']+1)))
+        create_concertim_device(concertimService, device_name=device[0], rack_id=new_device_location['rack_id'], start_location_id=new_device_location['start_u'], template_id=new_device_template['id'], device_description=device[1], facing='f')
 
 # Create a device in CONCERTIM with the passed args (facing is either f or b for front or back respectively, default will be front)
 def create_concertim_device(concertimService, device_name, rack_id, start_location_id, template_id, device_description, facing='f'):
@@ -114,16 +187,16 @@ def create_concertim_device(concertimService, device_name, rack_id, start_locati
     if concertimService._auth_token is not None:
         headers["Authorization"] = concertimService._auth_token
     else:
-        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: " + concertimService._auth_token)
+        raise Exception(f"No Authentication Token found in concertimService object - concertimService._auth_token is: {concertimService._auth_token}")
     try:
         response = requests.post(url, headers=headers, data=data, verify=False)
         if response.status_code in (200, 201):
             concertimService._logger.info("New Device Created Successfully")
             return response.json()
         else:
-            raise Exception("Response returned non 200 or 201 status code: ", response.text)
+            raise Exception(f"Response returned non 200 or 201 status code: {response.text}")
     except Exception as e:
-        concertimService._logger.exception("Failed to create new device in CONCERTIM API: ", e)
+        concertimService._logger.exception(f"Failed to create new device in CONCERTIM API: {e}")
 
 # Post a metric to concertim for a specific object (rack, device, etc)
 #   obj_to_update_name  =   the name of the rack/device/etc in concertim
@@ -148,13 +221,13 @@ def post_metric_to_concertim(concertimService, obj_to_update_name, metric_name, 
     if concertimService._auth_token is not None:
         headers["Authorization"] = concertimService._auth_token
     else:
-        raise Exception("No Authentication Token found in concertimService object - concertimService._auth_token is: ", concertimService._auth_token)
+        raise Exception(f"No Authentication Token found in concertimService object - concertimService._auth_token is: {concertimService._auth_token}")
     try:
         response = requests.put(url, headers=headers, data=data, verify=False)
         concertimService._logger.info(f"Metric Successfully Added: added {metric_name} to {obj_to_update_name}")
         return response.json()
     except Exception as e:
-        concertimService._logger.exception("Failed to put metric in CONCERTIM: ", e)
+        concertimService._logger.exception(f"Failed to put metric in CONCERTIM: {e}")
         raise e
         return
         
--- concertim/concertim_service.py
+++ concertim/concertim_service.py
@@ -3,6 +3,7 @@
 
 import json
 import requests
+import time
 requests.packages.urllib3.disable_warnings() 
 import logging
 import os
@@ -30,13 +31,13 @@ class ConcertimService(object):
         self._authenticate_openstack()
         self._connect_gnocchi()
         self._authenticate_concertim(self._config["concertim_username"], self._config["concertim_password"])
-        #while True:
-        #    self._update_concertim()
-        #    self._send_metrics()
-        #    time.sleep(self._config["interval"])
-        #print(concertim_helper.create_concertim_device(concertimService=self, device_name="concertim-instance-1", rack_id=1, start_location_id=42, template_id=2, device_description="b08e4798-4b1c-40bc-8068-f32fa0b5674f", facing="f"))
+        while True:
+            self._update_concertim()
+            self._send_metrics()
+            time.sleep(300)
+        #print(concertim_helper.create_concertim_device(concertimService=self, device_name="concertim-instance-4", rack_id=1, start_location_id=38, template_id=5, device_description="Made UP instance", facing="f"))
         #self._update_concertim()
-        self._send_metrics()
+        #self._send_metrics()
 
 
     # Loads the configuration from the specified JSON file
@@ -105,23 +106,44 @@ class ConcertimService(object):
     def _send_metrics(self):
         # LIMIT TO ONE PROJECT FOR TESTING
         project_id_list = ["ef821aa9e576420c8768671d911a9766"]
-        cpu_granularity = 60
+        ceilometer_granularity = 300
+        stop = datetime.now() - timedelta(minutes=60)
+        start = stop - timedelta(minutes=10)
         
-
         for project_id in project_id_list:
-            #print(self._gnocchi.aggregates.fetch(operations="(* (/ (aggregate rate:mean (metric cpu mean)) 60000000000.0) 100)", resource_type="instance"))
+            self._logger.info(f"Begin Sending Metrics for Project: {project_id}")
             project_query = {"and": [{"=":{"project_id":project_id}}, {"=":{"ended_at":None}}]}
-            openstack_instance_list = self._gnocchi.resource.search(resource_type="instance", query=project_query)
-            #print(openstack_instance_list)
-            for instance in openstack_instance_list:
-                cpu_metric_id = instance['metrics']['cpu']
-                stop = datetime.now() - timedelta(minutes=60)
-                start = stop - timedelta(minutes=5)
-                #print(self._gnocchi.metric.get(metric=cpu_metric_id))
-                #print(self._gnocchi.metric.aggregation(metrics=[cpu_metric_id], aggregation="mean", reaggregation="rate:mean", start=start, stop=stop))
-                print(start)
-                print(stop)
-                print(self._gnocchi.aggregates.fetch(operations=f"(* (/ (aggregate rate:mean (metric {cpu_metric_id} mean)) 300000000000.0) 100)", resource_type="instance", start=start, stop=stop))
+            openstack_resources_list = self._gnocchi.resource.search(query=project_query, details=True)
+            for resource in openstack_resources_list:
+                # Metric Fetching based on resource
+                if resource["type"] == "instance":
+
+                    # CPU Load as a percent
+                    self._logger.info(f"Calculating CPU Load for {resource['display_name']}")
+                    cpu_metric_id = resource['metrics']['cpu']
+                    ns_to_s_granularity = ceilometer_granularity * 1000000000.0
+                    cpu_load_percent = self._gnocchi.aggregates.fetch(operations=f"(* (/ (aggregate rate:mean (metric {cpu_metric_id} mean)) {ns_to_s_granularity}) 100)", 
+                                                                    resource_type="instance", start=start, stop=stop)["measures"]["aggregated"][0][2]
+                    self._logger.debug(f"Sending CPU Load = {cpu_load_percent} to instance {resource['id']} ({resource['display_name']})")
+                    concertim_helper.post_metric_to_concertim(concertimService=self, obj_to_update_name=resource['display_name'], metric_name=f"os.cpu_utilization", 
+                                                                    metric_value=cpu_load_percent, metric_datatype="float", metric_slope="both", metric_units="%")
+                    
+                    # RAM Usage as a percent
+                    self._logger.info(f"Calculating RAM Usage for {resource['display_name']}")
+                    memory_metric_id = resource['metrics']['memory']
+                    memory = self._gnocchi.metric.get_measures(metric=memory_metric_id, limit=1)[0][2]
+                    memory_usage_metric_id = resource['metrics']['memory.usage']
+                    memory_usage = self._gnocchi.metric.get_measures(metric=memory_usage_metric_id, start=start, stop=stop)[0][2]
+                    ram_usage_percent = memory_usage/memory*100
+                    self._logger.debug(f"Sending RAM Usage = {ram_usage_percent} to instance {resource['id']} ({resource['display_name']})")
+                    concertim_helper.post_metric_to_concertim(concertimService=self, obj_to_update_name=resource['display_name'], metric_name=f"os.ram_usage", 
+                                                                    metric_value=ram_usage_percent, metric_datatype="float", metric_slope="both", metric_units="%")
+
+                elif resource["type"] == "instance_network_interface":
+                    r_instance_network_interface = resource
+                
+                elif resource["type"] == "instance_disk":
+                    x = resource
 
     # Check openstack to see if there are any new instances and update concertim if there are
     def _update_concertim(self):
@@ -129,15 +151,19 @@ class ConcertimService(object):
         project_query = {"and": [{"=":{"project_id":"ef821aa9e576420c8768671d911a9766"}}, {"=":{"ended_at":None}}]}
         new_rack_height = 42
 
-        openstack_instance_list = self._gnocchi.resource.search(resource_type="instance", query=project_query)
+        openstack_instance_list = self._gnocchi.resource.search(resource_type="instance", query=project_query, details=True)
         concertim_device_list = concertim_helper.get_concertim_devices(concertimService=self)
         concertim_rack_list = concertim_helper.get_concertim_racks(concertimService=self)
+        new_device_list = []
 
         for instance in openstack_instance_list:
             corresponding_device = [d for d in concertim_device_list if d['name'] == instance["display_name"]]
             if not corresponding_device:
-                print("create device " + instance["display_name"] + " here")
+                self._logger.info(f"New Instance Found in Openstack - {instance['display_name']}")
+                instance_vcpus = self._gnocchi.metric.get_measures(metric=instance["metrics"]["vcpus"])[0][2]
+                new_device_list.append((instance["display_name"], instance["id"], instance_vcpus))
 
+        concertim_helper.build_device_list(self,new_device_list)
 
     # Handles exceptions that occur during runtime
     def _handle_exception(self, e):
