625c90b2d98f87a55f9cfc7d3f5c235345ca0fe7
Logan Smith <logan.smith8426@gmail.com>
2023-08-17 21:43:54 +0100

    get proper root

--- con_opstk/app_definitions.py
+++ con_opstk/app_definitions.py
@@ -1,7 +1,7 @@
 import os
 
 # Calculated paths
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 LOG_DIR = os.path.join(ROOT_DIR, '/var/log/')
 DATA_DIR = os.path.join(ROOT_DIR, '/var/data/')
 
