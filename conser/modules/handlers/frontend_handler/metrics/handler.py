96c372c1d1b41e5cac49fd08938fcaada8b2be8e
timalces <tim.brackfield@alces-software.com>
2024-05-09 17:13:12 +0000

    use device type instead of device details type

--- conser/modules/handlers/frontend_handler/metrics/handler.py
+++ conser/modules/handlers/frontend_handler/metrics/handler.py
@@ -76,7 +76,7 @@ class MetricsHandler(Handler):
                 self.__LOGGER.debug(f"Skipping metrics for device {device_id_tup}")
                 continue
             self.__LOGGER.debug(f"Starting -- Updating metrics for device {device_id_tup}")
-            if device.details['type'] != 'Device::ComputeDetails':
+            if device.type != 'Instance':
                 self.__LOGGER.debug(f"Device is not a server and is unsupported - skipping {device_id_tup}")
                 continue
             metrics = self.clients['cloud'].get_metrics(
--- conser/modules/handlers/frontend_handler/updates/handler.py
+++ conser/modules/handlers/frontend_handler/updates/handler.py
@@ -129,7 +129,7 @@ class UpdatesHandler(Handler):
                     self.__LOGGER.debug(f"Device not found in Concertim - Cannot create device - waiting for rack to be created first - skipping")
                     continue
                 self.__LOGGER.debug(f"Device not found in Concertim - creating device {device_id_tup}")
-                created_device = self.create_new_device(device)
+                created_device = self.create_new_device(device) # TODO: investigate why this not working
                 device.id = (created_device['id'], device_id_tup[1])
             else:
                 self.__LOGGER.warning(f"Unrecognized device found in view {device}")
@@ -252,7 +252,7 @@ class UpdatesHandler(Handler):
                 "openstack_stack_id": device_obj.rack_id_tuple[1]
             }
             try:
-                if device_obj.details['type'] == 'Device::ComputeDetails':
+                if device_obj.type == 'Instance':
                     self.clients['concertim'].update_compute_device(
                         ID=device_obj.id[0],
                         variables_dict={
@@ -265,7 +265,7 @@ class UpdatesHandler(Handler):
                             "net_interfaces": device_obj.network_interfaces,
                         }
                     )
-                elif device_obj.details['type'] == 'Device::VolumeDetails':
+                elif device_obj.type == 'Volume':
                     self.clients['concertim'].update_volume_device(
                         ID=device_obj.id[0],
                         variables_dict={
@@ -273,7 +273,7 @@ class UpdatesHandler(Handler):
                             **device_obj.details
                         }
                     )
-                elif device_obj.details['type'] == 'Device::NetworkDetails':
+                elif device_obj.type == 'Network':
                     self.clients['concertim'].update_network_device(
                         ID=device_obj.id[0],
                         variables_dict={
@@ -282,7 +282,7 @@ class UpdatesHandler(Handler):
                         }
                     )
                 else:
-                    self.__LOGGER.error(f"Asked to update device of type {device_obj.details['type']} but don't know how")
+                    self.__LOGGER.error(f"Asked to update device of type {device_obj.type} but don't know how")
                 device_obj._updated = False
                 # "But wait! If we fall into the default case above, the device hasn't really been updated!"
                 # That's true, but running the loop more times isn't going to change that. We've logged the problem,
@@ -308,7 +308,7 @@ class UpdatesHandler(Handler):
             "openstack_stack_id": self.view.racks[device_obj.rack_id_tuple].id[1],
         }
         try:
-            if device_obj.details['type'] == 'Device::ComputeDetails':
+            if device_obj.type == 'Instance':
                 return self.clients['concertim'].create_compute_device(
                     variables_dict={
                         **common_vars,
@@ -320,14 +320,14 @@ class UpdatesHandler(Handler):
                         "login_user": device_obj.details.get('login_user'),
                     }
                 )
-            elif device_obj.details['type'] == 'Device::VolumeDetails':
+            elif device_obj.type == 'Volume':
                 return self.clients['concertim'].create_volume_device(
                     variables_dict={
                         **common_vars,
                         **device_obj.details
                     }
                 )
-            elif device_obj.details['type'] == 'Device::NetworkDetails':
+            elif device_obj.type == 'Network':
                 return self.clients['concertim'].create_network_device(
                     variables_dict={
                         **common_vars,
